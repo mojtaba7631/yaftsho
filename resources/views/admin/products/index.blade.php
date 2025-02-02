482902a725ce70523374e19ecbcc35c0c08aa9d4
Mojtaba Azizi <mojtabaazizi7631@gmail.com>
2025-02-02 17:32:29 +0330

    inser summernote but is bug

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -4,6 +4,14 @@
     مدیریت محصولات
 @endsection
 
+@section('custom_js')
+    <script>
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>
+@endsection
+
 @section('dashboard_content')
     <div class="container-xl">
         <div class="card">
@@ -19,17 +27,13 @@
                                 <button class="table-sort" data-sort="sort-city">نام محصول</button>
                             </th>
                             <th>
-                                <button class="table-sort" data-sort="sort-type">Type</button>
+                                <button class="table-sort" data-sort="sort-type">نوع محصول</button>
                             </th>
                             <th>
-                                <button class="table-sort" data-sort="sort-score">Score</button>
+                                <button class="table-sort" data-sort="sort-score">قیمت</button>
                             </th>
-                            <th>
-                                <button class="table-sort" data-sort="sort-date">Date</button>
-                            </th>
-                            <th>
-                                <button class="table-sort" data-sort="sort-quantity">Quantity</button>
-                            </th>
+
+
                             <th>
                                 <button class="table-sort" data-sort="sort-progress">عملیات</button>
                             </th>
@@ -38,25 +42,20 @@
                         <tbody class="table-tbody">
                         @foreach($products as $product)
                             <tr>
-                                <td class="sort-name">Steel Vengeance</td>
-                                <td class="sort-city">Cedar Point, United States</td>
-                                <td class="sort-type">RMC Hybrid</td>
-                                <td class="sort-score">100,0%</td>
-                                <td class="sort-date" data-date="1730273409">October 30, 2024</td>
-                                <td class="sort-quantity">74</td>
-                                <td class="sort-progress" data-progress="30">
-                                    <div class="row align-items-center">
-                                        <div class="col-12 col-lg-auto">30%</div>
-                                        <div class="col">
-                                            <div class="progress progress-2" style="width: 5rem">
-                                                <div class="progress-bar" style="width: 30%" role="progressbar"
-                                                     aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"
-                                                     aria-label="30% Complete">
-                                                    <span class="visually-hidden">30% Complete</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                <td class="sort-name"><img src="#"></td>
+                                <td class="sort-city">{{$product['title']}}</td>
+                                <td class="sort-type">{{$product['type']}}</td>
+                                <td class="sort-score">{{@number_format($product['price'])}}</td>
+                                <td>
+
+                                </td>
+                                <td class="sort-progress">
+                                    <button class="btn btn-sm btn-primary">
+                                        ویرایش محصول
+                                    </button>
+                                    <button class="btn btn-sm btn-danger">
+                                        حذف محصول
+                                    </button>
                                 </td>
                             </tr>
                         @endforeach
@@ -79,4 +78,7 @@
             })
         </script>
     </div>
+
+    <div id="summernote"></div>
+
 @endsection
--- resources/views/base/base_layout_tabler.blade.php
+++ resources/views/base/base_layout_tabler.blade.php
@@ -1,11 +1,14 @@
 <!doctype html>
-<html lang="en"  dir="rtl" >
+<html lang="en" dir="rtl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
         @yield('title')
     </title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('summernote/summernote.css')}}">
+
     @vite('resources/css/tabler_rtl.css')
     @yield('custom_css')
     @livewireStyles
@@ -13,6 +16,10 @@
 <body>
 
 @yield('content')
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="{{asset('summernote/summernote.js')}}"></script>
 
 @vite('resources/js/tabler.js')
 @livewireScripts
