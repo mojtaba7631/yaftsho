d140d33cb601ced507233ff2f7c9d9a6c426e7dc
Mojtaba Azizi <mojtabaazizi7631@gmail.com>
2025-02-02 15:32:56 +0330

    last upload

---
+++ resources/views/seller/dashboard/index.blade.php
@@ -0,0 +1,13 @@
+@extends('base.base_layout_seller')
+
+@section('title')
+    seller dashboard
+@endsection
+
+@section('dashboard_content')
+    <h1>
+        main seller content
+    </h1>
+@endsection
+
+
---
+++ resources/views/user/dashboard/index.blade.php
@@ -0,0 +1,13 @@
+@extends('base.base_layout_user')
+
+@section('title')
+    user dashboard
+@endsection
+
+@section('dashboard_content')
+    <h1>
+        main user content
+    </h1>
+@endsection
+
+
